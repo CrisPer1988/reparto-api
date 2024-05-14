8229a2ca2b2076bb9d62b4bb791c90555ea31230
CristianPerez <norberto.cp@hotmail.com>
2024-05-14 11:55:55 -0300

    url routes updates

--- src/app.js
+++ src/app.js
@@ -17,10 +17,10 @@ const commerceRouter = require("./routes/commerce.routes");
 const productRouter = require("./routes/products.routes");
 const OrderRouter = require("./routes/orders.routes");
 
-app.use("/api/v1/users", userRouter);
-app.use("/api/v1/commerces", commerceRouter);
-app.use("/api/v1/products", productRouter);
-app.use("/api/v1/orders", OrderRouter);
+app.use("/users", userRouter);
+app.use("/commerces", commerceRouter);
+app.use("/products", productRouter);
+app.use("/orders", OrderRouter);
 app.use("/downloads", express.static(path.join(__dirname, "downloads")));
 
 module.exports = app;
