2cdfe5988deb27eea0ebedcb7a2c257598061e58
CristianPerez <norberto.cp@hotmail.com>
2024-07-31 09:38:47 -0300

    change uploads local

--- src/distributor/routes/index.js
+++ src/distributor/routes/index.js
@@ -1,21 +1,69 @@
+// const express = require("express");
+// const multer = require("multer");
+
+// const distributorController = require("../controllers");
+// const validations = require("../middlewares");
+// const validationsOwner = require("../../owner/middlewares");
+// const upload = multer({ dest: "uploads/" });
+
+// const router = express.Router();
+
+// router.route("/login").post(distributorController.loginDistributor);
+// router
+//   .route("/:id")
+//   .post(
+//     validationsOwner.validExistsOwner,
+//     upload.single("file"),
+//     distributorController.createDistributor
+//   );
+// router
+//   .route("/:id")
+//   .get(
+//     validations.validExistsDistributor,
+//     distributorController.findDistributor
+//   );
+
+// //admin
+// router
+//   .route("/:id/admin")
+//   .post(validations.validExistsDistributor, distributorController.createAdmin);
+
+// module.exports = router;
+
 const express = require("express");
 const multer = require("multer");
+const path = require("path");
 
 const distributorController = require("../controllers");
 const validations = require("../middlewares");
 const validationsOwner = require("../../owner/middlewares");
-const upload = multer({ dest: "uploads/" });
 
+// Configuración de multer para almacenar archivos en /tmp/uploads
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join("/tmp", "uploads")); // Asegúrate de que la carpeta existe
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname); // Usar el nombre original del archivo
+  },
+});
+
+const upload = multer({ storage: storage });
+
+// Crear el router de Express
 const router = express.Router();
 
+// Ruta para login del distribuidor
 router.route("/login").post(distributorController.loginDistributor);
-router
-  .route("/:id")
-  .post(
-    validationsOwner.validExistsOwner,
-    upload.single("file"),
-    distributorController.createDistributor
-  );
+
+// Ruta para crear un distribuidor con validaciones y subida de archivo
+router.route("/:id").post(
+  validationsOwner.validExistsOwner,
+  upload.single("file"), // Middleware de multer para manejar la subida del archivo
+  distributorController.createDistributor
+);
+
+// Ruta para obtener un distribuidor con validaciones
 router
   .route("/:id")
   .get(
@@ -23,7 +71,7 @@ router
     distributorController.findDistributor
   );
 
-//admin
+// Ruta para crear un admin con validaciones
 router
   .route("/:id/admin")
   .post(validations.validExistsDistributor, distributorController.createAdmin);
--- src/products/productDetails/routes/index.js
+++ src/products/productDetails/routes/index.js
@@ -1,11 +1,40 @@
+// const express = require("express");
+// const multer = require("multer");
+
+// const controllers = require("../controller");
+// const productsValidations = require("../../middlewares");
+
+// const router = express.Router();
+// const upload = multer({ dest: "uploads/" });
+
+// router
+//   .route("/:id")
+//   .post(
+//     productsValidations.validExistsProduct,
+//     upload.single("file"),
+//     controllers.createProductDetails
+//   );
+
+// router
+//   .route("/product/:id")
+//   .get(
+//     productsValidations.validExistsProduct,
+//     controllers.allProductDetailsByProduct
+//   );
+
+// // router.route("/").get(controllers.allProducts);
+
+// module.exports = router;
+
 const express = require("express");
 const multer = require("multer");
+const path = require("path");
 
 const controllers = require("../controller");
 const productsValidations = require("../../middlewares");
 
 const router = express.Router();
-const upload = multer({ dest: "uploads/" });
+const upload = multer({ dest: path.join("/tmp", "uploads") });
 
 router
   .route("/:id")
